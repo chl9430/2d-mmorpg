7059fc9af8ef92a853807e2ae8dbef545bb443b5
JUNG HOO CHOI <chl9430@gmail.com>
2025-02-19 17:22:34 +0900

    맵 동적 생성 및 콜라이더 적용.

--- 2DGame/Assets/Editor/MapEditor.cs
+++ 2DGame/Assets/Editor/MapEditor.cs
@@ -14,8 +14,8 @@ public class MapEditor
 {
 
 #if UNITY_EDITOR
-    // 단축키 옵션
-    // % Ctrl, # Shift, & Alt
+    // 에디터 상단 탭에 새로운 메뉴를 추가한다.
+    // 단축키 옵션 : % - Ctrl, # - Shift, & - Alt
     [MenuItem("Tools/GenerateMap %#g")]
     private static void GenerateMap()
     {
@@ -23,20 +23,21 @@ public class MapEditor
 
         foreach (GameObject go in gameObjects)
         {
+            Tilemap tmBase = Util.FindChild<Tilemap>(go, "Tilemap_Base", true);
             Tilemap tm = Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
 
             // 파일 생성
             using (var writer = File.CreateText($"Assets/Resources/Map/{go.name}.txt"))
             {
-                writer.WriteLine(tm.cellBounds.xMin);
-                writer.WriteLine(tm.cellBounds.xMax);
-                writer.WriteLine(tm.cellBounds.yMin);
-                writer.WriteLine(tm.cellBounds.yMax);
+                writer.WriteLine(tmBase.cellBounds.xMin);
+                writer.WriteLine(tmBase.cellBounds.xMax);
+                writer.WriteLine(tmBase.cellBounds.yMin);
+                writer.WriteLine(tmBase.cellBounds.yMax);
 
                 // 타일 맵을 순회하며, 해당위치에 타일이 존재한다면 1, 존재하지 않다면 0을 출력한다.
-                for (int y = tm.cellBounds.yMax; y >= tm.cellBounds.yMin; y--)
+                for (int y = tmBase.cellBounds.yMax; y >= tmBase.cellBounds.yMin; y--)
                 {
-                    for (int x = tm.cellBounds.xMin; x <= tm.cellBounds.xMax; x++)
+                    for (int x = tmBase.cellBounds.xMin; x <= tmBase.cellBounds.xMax; x++)
                     {
                         TileBase tile = tm.GetTile(new Vector3Int(x, y, 0));
                         if (tile != null)
---
+++ 2DGame/Assets/Scripts/Managers/Contents/MapManager.cs
@@ -0,0 +1,75 @@
+using System.IO;
+using UnityEngine;
+
+public class MapManager
+{
+    public Grid CurrentGrid { get; private set; }
+
+    public int MinX { get; set; }
+    public int MaxX { get; set; }
+    public int MinY { get; set; }
+    public int MaxY { get; set; }
+
+    bool[,] _collision;
+
+    public bool CanGo(Vector3Int cellPos)
+    {
+        if (cellPos.x < MinX || cellPos.x > MaxX)
+            return false;
+        if (cellPos.y < MinY || cellPos.y > MaxY)
+            return false;
+
+        int x = cellPos.x - MinX;
+        int y = MaxY - cellPos.y;
+        return !_collision[y, x];
+    }
+
+    public void LoadMap(int mapId)
+    {
+        DestroyMap();
+
+        string mapName = "Map_" + mapId.ToString("000");
+        GameObject go = Managers.Resource.Instantiate($"Map/{mapName}");
+        go.name = "Map";
+
+        // 맵을 씬에 인스턴스 한 뒤 콜리젼 타일맵을 안보이게 비활성화 한다.
+        GameObject collision = Util.FindChild(go, "Tilemap_Collision", true);
+        if (collision != null)
+            collision.SetActive(false);
+
+        CurrentGrid = go.GetComponent<Grid>();
+
+        // Collision 관련 파일
+        TextAsset txt = Managers.Resource.Load<TextAsset>($"Map/{mapName}");
+        StringReader reader = new StringReader(txt.text);
+
+        MinX = int.Parse(reader.ReadLine());
+        MaxX = int.Parse(reader.ReadLine());
+        MinY = int.Parse(reader.ReadLine());
+        MaxY = int.Parse(reader.ReadLine());
+
+        int xCount = MaxX - MinX + 1;
+        int yCount = MaxY - MinY + 1;
+        _collision = new bool[yCount, xCount];
+
+        // 타일맵의 각 타일에 해당하는 2차원 배열의 인덱스를 채운다.
+        for (int y = 0; y < yCount; y++)
+        {
+            string line = reader.ReadLine();
+            for (int x = 0; x < xCount; x++)
+            {
+                _collision[y, x] = (line[x] == '1' ? true : false);
+            }
+        }
+    }
+
+    public void DestroyMap()
+    {
+        GameObject map = GameObject.Find("Map");
+        if (map != null)
+        {
+            GameObject.Destroy(map);
+            CurrentGrid = null;
+        }
+    }
+}
