11eb0883773c9e02fb1502170e2b7c7f63641cea
JUNG HOO CHOI <chl9430@gmail.com>
2025-03-04 05:19:19 +0900

    dll 제거 및 필요한 소스코드만 추가하여 보안성 강화. 멀티플레이 환경 테스트를 위한 원하는 클라이언트 수만큼 빌드 후 실행 자동화.

---
+++ 2DGame/Assets/Editor/MultiplayersBuildAndRun.cs
@@ -0,0 +1,56 @@
+using UnityEditor;
+using UnityEngine;
+
+public class MultiplayersBuildAndRun
+{
+    [MenuItem("Tools/Run Multiplayer/2 Players")]
+    static void PerformWin64Build2()
+    {
+        PerformWin64Build(2);
+    }
+
+    [MenuItem("Tools/Run Multiplayer/3 Players")]
+    static void PerformWin64Build3()
+    {
+        PerformWin64Build(3);
+    }
+
+    [MenuItem("Tools/Run Multiplayer/4 Players")]
+    static void PerformWin64Build4()
+    {
+        PerformWin64Build(4);
+    }
+
+    static void PerformWin64Build(int playerCount)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(
+            BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows);
+
+        for (int i = 1; i <= playerCount; i++)
+        {
+            // 빌드 폴더를 여러개 만든다.
+            BuildPipeline.BuildPlayer(GetScenePaths(),
+                "Builds/Win64/" + GetProjectName() + i.ToString() + "/" + GetProjectName() + i.ToString() + ".exe",
+                BuildTarget.StandaloneWindows64, BuildOptions.AutoRunPlayer);
+            // 자동으로 실행하게 만든다.
+        }
+    }
+
+    static string GetProjectName()
+    {
+        string[] s = Application.dataPath.Split('/');
+        return s[s.Length - 2];
+    }
+
+    static string[] GetScenePaths()
+    {
+        string[] scenes = new string[EditorBuildSettings.scenes.Length];
+
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            scenes[i] = EditorBuildSettings.scenes[i].path;
+        }
+
+        return scenes;
+    }
+}
