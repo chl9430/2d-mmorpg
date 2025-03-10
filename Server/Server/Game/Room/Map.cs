b55cf02af7005a46a225e3aa1f342f0b9d63d885
JUNG HOO CHOI <chl9430@gmail.com>
2025-03-10 13:13:58 +0900

    GameObject 클래스 생성. 서버 및 클라이언트에서 화살 발사처리.

--- Server/Server/Game/Room/Map.cs
+++ Server/Server/Game/Room/Map.cs
@@ -60,7 +60,7 @@ namespace Server.Game
         public int SizeY { get { return MaxY - MinY + 1; } }
 
         bool[,] _collision;
-        Player[,] _players;
+        GameObject[,] _objects;
 
         public bool CanGo(Vector2Int cellPos, bool checkObjects = true)
         {
@@ -71,10 +71,10 @@ namespace Server.Game
 
             int x = cellPos.x - MinX;
             int y = MaxY - cellPos.y;
-            return !_collision[y, x] && (!checkObjects || _players[y, x] == null);
+            return !_collision[y, x] && (!checkObjects || _objects[y, x] == null);
         }
 
-        public Player Find(Vector2Int cellPos)
+        public GameObject Find(Vector2Int cellPos)
         {
             if (cellPos.x < MinX || cellPos.x > MaxX)
                 return null;
@@ -83,30 +83,39 @@ namespace Server.Game
 
             int x = cellPos.x - MinX;
             int y = MaxY - cellPos.y;
-            return _players[y, x];
+            return _objects[y, x];
         }
 
-        public bool ApplyMove(Player player, Vector2Int dest)
+        public bool ApplyLeave(GameObject gameObject)
         {
-            PositionInfo posInfo = player.Info.PosInfo;
+            PositionInfo posInfo = gameObject.PosInfo;
             if (posInfo.PosX < MinX || posInfo.PosX > MaxX)
                 return false;
             if (posInfo.PosY < MinY || posInfo.PosY > MaxY)
                 return false;
 
+            {
+                int x = posInfo.PosX - MinX;
+                int y = MaxY - posInfo.PosY;
+                if (_objects[y, x] == gameObject)
+                    _objects[y, x] = null;
+            }
+
+            return true;
+        }
+
+        public bool ApplyMove(GameObject gameObject, Vector2Int dest)
+        {
+            ApplyLeave(gameObject);
+            PositionInfo posInfo = gameObject.PosInfo;
+
             if (CanGo(dest, true) == false)
                 return false;
 
             {
-                int x = posInfo.PosX - MinX;
-                int y = MaxY - posInfo.PosY;
-                if (_players[y, x] == player)
-                    _players[y, x] = null;
-            }
-            {
                 int x = dest.x - MinX;
                 int y = MaxY - dest.y;
-                _players[y, x] = player;
+                _objects[y, x] = gameObject;
             }
 
             // 실제 좌표 이동
@@ -132,7 +141,7 @@ namespace Server.Game
             int xCount = MaxX - MinX + 1;
             int yCount = MaxY - MinY + 1;
             _collision = new bool[yCount, xCount];
-            _players = new Player[yCount, xCount];
+            _objects = new GameObject[yCount, xCount];
 
             // 타일맵의 각 타일에 해당하는 2차원 배열의 인덱스를 채운다.
             for (int y = 0; y < yCount; y++)
