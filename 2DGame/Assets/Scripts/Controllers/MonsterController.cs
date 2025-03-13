151076b2abb67e4c75dbeaa5024a3d05131ceb59
JUNG HOO CHOI <chl9430@gmail.com>
2025-03-13 12:10:35 +0900

    서버에서 몬스터를 관리하고 생성하여 플레이어를 추적하도록 ai 구현.

--- 2DGame/Assets/Scripts/Controllers/MonsterController.cs
+++ 2DGame/Assets/Scripts/Controllers/MonsterController.cs
@@ -7,47 +7,9 @@ using static Define;
 public class MonsterController : CreatureController
 {
     Coroutine _coSkill;
-    Coroutine _coPatrol;
-    Coroutine _coSearch;
-
-    [SerializeField]
-    Vector3Int _destCellPos;
-
-    [SerializeField]
-    GameObject _target;
-
-    [SerializeField]
-    float _searchRange = 10.0f;
-
-    [SerializeField]
-    float _skillRange = 1.0f;
 
     [SerializeField]
     bool _rangedSkill = false;
-
-    public override CreatureState State
-    {
-        get { return PosInfo.State; }
-        set
-        {
-            if (PosInfo.State == value)
-                return;
-
-            base.State = value;
-
-            if (_coPatrol != null)
-            {
-                StopCoroutine(_coPatrol);
-                _coPatrol = null;
-            }
-
-            if (_coSearch != null)
-            {
-                StopCoroutine(_coSearch);
-                _coSearch = null;
-            }
-        }
-    }
 
     protected override void Init()
     {
@@ -57,129 +19,17 @@ public class MonsterController : CreatureController
         Dir = MoveDir.Down;
 
         _rangedSkill = (Random.Range(0, 2) == 0 ? true : false);
-
-        if (_rangedSkill)
-            _skillRange = 10.0f;
-        else
-            _skillRange = 1.0f;
     }
 
     protected override void UpdateIdle()
     {
         base.UpdateIdle();
-
-        if (_coPatrol == null)
-        {
-            _coPatrol = StartCoroutine("CoPatrol");
-        }
-
-        if (_coSearch == null)
-        {
-            _coSearch = StartCoroutine("CoSearch");
-        }
-    }
-
-    protected override void MoveToNextPos()
-    {
-        Vector3Int destPos = _destCellPos;
-        if (_target != null)
-        {
-            destPos = _target.GetComponent<CreatureController>().CellPos;
-
-            Vector3Int dir = destPos - CellPos;
-            if (dir.magnitude <= _skillRange && (dir.x == 0 || dir.y == 0))
-            {
-                Dir = GetDirFromVec(dir);
-                State = CreatureState.Skill;
-
-                if (_rangedSkill)
-                    _coSkill = StartCoroutine("CoStartShootArrow");
-                else
-                    _coSkill = StartCoroutine("CoStartPunch");
-
-                return;
-            }
-        }
-
-        List<Vector3Int> path = Managers.Map.FindPath(CellPos, destPos, ignoreDestCollision: true);
-
-        // 플레이어로 향하는 길을 못 찾았거나 (중간에 장애물 등이 있을 때)
-        // 플레이어가 멀어졌을때
-        if (path.Count < 2 || (_target != null && path.Count > 20))
-        {
-            _target = null;
-            State = CreatureState.Idle;
-            return;
-        }
-
-        Vector3Int nextPos = path[1];
-
-        Vector3Int moveCellDir = nextPos - CellPos;
-
-        Dir = GetDirFromVec(moveCellDir);
-
-        // 가게 될 곳이 갈 수 있는지 확인한다.
-        if (Managers.Map.CanGo(nextPos) && Managers.Object.FindCreature(nextPos) == null)
-        {
-            CellPos = nextPos;
-        }
-        else
-        {
-            State = CreatureState.Idle;
-        }
     }
 
     public override void OnDamaged()
     {
-        Managers.Object.Remove(Id);
-        Managers.Resource.Destroy(gameObject);
-    }
-
-    IEnumerator CoPatrol()
-    {
-        int waitSeconds = Random.Range(1, 4);
-        yield return new WaitForSeconds(waitSeconds);
-
-        for (int i = 0; i < 10; i++)
-        {
-            int xRange = Random.Range(-5, 6);
-            int yRange = Random.Range(-5, 6);
-            Vector3Int randPos = CellPos + new Vector3Int(xRange, yRange, 0);
-
-            if (Managers.Map.CanGo(randPos) && Managers.Object.FindCreature(randPos) == null)
-            {
-                _destCellPos = randPos;
-                State = CreatureState.Moving;
-                yield break;
-            }
-        }
-
-        State = CreatureState.Idle;
-    }
-
-    IEnumerator CoSearch()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(1);
-
-            if (_target != null)
-                continue;
-
-            // 일정한 사정거리 내 타겟을 찾아 멤버 변수에 할당한다.
-            _target = Managers.Object.Find((go) =>
-            {
-                PlayerController pc = go.GetComponent<PlayerController>();
-                if (pc == null)
-                    return false;
-
-                Vector3Int dir = (pc.CellPos - CellPos);
-                if (dir.magnitude > _searchRange)
-                    return false;
-
-                return true;
-            });
-        }
+        //Managers.Object.Remove(Id);
+        //Managers.Resource.Destroy(gameObject);
     }
 
     IEnumerator CoStartPunch()
--- Server/Server/Game/Object/Monster.cs
+++ Server/Server/Game/Object/Monster.cs
@@ -12,6 +12,114 @@ namespace Server.Game
         public Monster()
         {
             ObjectType = GameObjectType.Monster;
+
+            Stat.Level = 1;
+            Stat.Hp = 100;
+            Stat.MaxHp = 100;
+            Stat.Speed = 5.0f;
+
+            State = CreatureState.Idle;
+        }
+
+        // FSM (Finite State Machine)
+        public override void Update()
+        {
+            switch (State)
+            {
+                case CreatureState.Idle:
+                    UpdateIdle();
+                    break;
+                case CreatureState.Moving:
+                    UpdateMoving();
+                    break;
+                case CreatureState.Skill:
+                    UpdateSkill();
+                    break;
+                case CreatureState.Dead:
+                    UpdateDead();
+                    break;
+            }
+        }
+
+        Player _target;
+        int _searchCellDist = 10;
+        int _chaseCellDist = 20;
+
+        long _nextSearchTick = 0;
+        protected virtual void UpdateIdle()
+        {
+            if (_nextSearchTick > Environment.TickCount64)
+                return;
+            _nextSearchTick = Environment.TickCount64 + 1000;
+
+            // 사정거리 내에 플레이어가 있는지 체크
+            Player target = Room.FindPlayer(p =>
+            {
+                Vector2Int dir = p.CellPos - CellPos;
+                return dir.cellDistFromZero <= _searchCellDist;
+            });
+
+            if (target == null)
+                return;
+
+            _target = target;
+            State = CreatureState.Moving;
+        }
+
+        long _nextMoveTick = 0;
+        protected virtual void UpdateMoving()
+        {
+            if (_nextMoveTick > Environment.TickCount64)
+                return;
+
+            int moveTick = (int)(1000 / Speed);
+            _nextMoveTick = Environment.TickCount64 + 1000;
+
+            // 쫒고 있는 플레이어가 도중에 해당 룸에서 나가버리면
+            if (_target == null || _target.Room != Room)
+            {
+                _target = null;
+                State = CreatureState.Idle;
+                return;
+            }
+
+            int dist = (_target.CellPos - CellPos).cellDistFromZero;
+            // 플레이어가 너무 빠르게 도망가버리면(몬스터가 쫒기를 포기하는 상황)
+            if (dist == 0 || dist > _chaseCellDist)
+            {
+                _target = null;
+                State = CreatureState.Idle;
+                return;
+            }
+
+            List<Vector2Int> path = Room.Map.FindPath(CellPos, _target.CellPos, checkObjects: false);
+            // 찾은 경로가 없거나 너무 멀다면
+            if (path.Count < 2 || path.Count > _chaseCellDist)
+            {
+                _target = null;
+                State = CreatureState.Idle;
+                return;
+            }
+
+            // 이동
+            Dir = GetDirFromVec(path[1] - CellPos);
+            Room.Map.ApplyMove(this, path[1]);
+
+            // 다른 플레이어한테도 알려준다.
+            S_Move movePacket = new S_Move();
+            movePacket.ObjectId = Id;
+            movePacket.PosInfo = PosInfo;
+            Room.Broadcast(movePacket);
+        }
+
+        protected virtual void UpdateSkill()
+        {
+
+        }
+
+        protected virtual void UpdateDead()
+        {
+
         }
     }
 }
